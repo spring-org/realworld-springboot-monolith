9c7bb9fe42d6c33ef7edda51609ed7e9995c8fa6
seokrae <kslbsh@gmail.com>
2021-09-30 12:17:30 +0900

    feat: article api 테스트 코드 작성

---
+++ src/test/java/com/example/realworld/application/articles/presentation/ArticlesApiTest.java
@@ -0,0 +1,9 @@
+package com.example.realworld.application.articles.presentation;
+
+import com.example.realworld.application.ControllerTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ArticlesApiTest extends ControllerTest {
+
+}
