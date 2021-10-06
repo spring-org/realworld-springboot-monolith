3e7e11088610252bad09119d2c9b072ad3cc4357
seokrae <kslbsh@gmail.com>
2021-10-06 19:23:48 +0900

    feat: 다시 tag

--- src/test/java/com/example/realworld/application/tags/presentation/TagApiTest.java
+++ src/test/java/com/example/realworld/application/tags/presentation/TagApiTest.java
@@ -1,5 +1,6 @@
 package com.example.realworld.application.tags.presentation;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -17,6 +18,7 @@ class TagApiTest {
     @Autowired
     public MockMvc mockMvc;
 
+    @DisplayName("태그 조회")
     @Test
     void testCase1() throws Exception {
         mockMvc.perform(get("/api/tags"))
