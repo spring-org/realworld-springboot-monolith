f3ed57828deba8fd80ae505967ac2e1a59531cc1
seokrae <kslbsh@gmail.com>
2021-10-06 17:23:38 +0900

    feat: tag api 테스트 코드 리펙토링

--- src/test/java/com/example/realworld/application/tags/presentation/TagApiTest.java
+++ src/test/java/com/example/realworld/application/tags/presentation/TagApiTest.java
@@ -1,26 +1,46 @@
 package com.example.realworld.application.tags.presentation;
 
+import com.example.realworld.application.BaseSpringBootTest;
+import com.example.realworld.application.tags.persistence.Tag;
+import com.example.realworld.application.tags.persistence.repository.TagRepository;
+import com.example.realworld.application.tags.service.TagService;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.List;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@SpringBootTest
-@AutoConfigureMockMvc
-class TagApiTest {
+
+class TagApiTest extends BaseSpringBootTest {
 
     @Autowired
-    public MockMvc mockMvc;
+    private TagService tagService;
+    @Autowired
+    private TagRepository tagRepository;
 
+    @AfterEach
+    void tearDown() {
+        tagRepository.deleteAll();
+    }
+
+    @DisplayName("전체 태그리스트 조회")
     @Test
-    void testCase1() throws Exception {
+    void when_getTags_expect_success_tagList() throws Exception {
+        tagRepository.saveAll(List.of(Tag.of("Java"), Tag.of("R"), Tag.of("Python"), Tag.of("Node.js"), Tag.of("JavaScript"), Tag.of("Ruby")));
+
         mockMvc.perform(get("/api/tags"))
-                .andDo(print())
-                .andExpect(status().isOk());
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.tagList.[0]").value("Java"))
+                .andExpect(jsonPath("$.tagList.[1]").value("JavaScript"))
+                .andExpect(jsonPath("$.tagList.[2]").value("Node.js"))
+                .andExpect(jsonPath("$.tagList.[3]").value("Python"))
+                .andExpect(jsonPath("$.tagList.[4]").value("R"))
+                .andExpect(jsonPath("$.tagList.[5]").value("Ruby"))
+        ;
     }
 }
